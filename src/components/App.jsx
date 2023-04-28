7b26a88a337cd5370078a0163d0a7ae209839872
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-04-28 19:02:22 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,6 @@ import { Profile } from "./Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
 import user from '../user.json';
 import data from '../data.json'
-import { Statlist } from "./Statistics/statlist";
-
-
 
 export const App = () => {
   
